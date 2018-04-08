f8088234fda5aee9d09820c5f8e68d3e1c672085
Kallyne <36283393+Kallyne@users.noreply.github.com>
2018-04-08 16:51:12 -0300

    Update Coordenador.sql

--- BD/Controle de Acesso/Coordenador.sql
+++ BD/Controle de Acesso/Coordenador.sql
@@ -1,12 +1,12 @@
 --Coordenador--
-create table Coordenador (
+CREATE TABLE Coordenador (
  IdCoordenador INT not null IDENTITY(1,1)
 ,Login INT not null 
 ,Senha VARCHAR(10) not null
 ,Nome VARCHAR(50) not null
 ,Email VARCHAR(50) not null 
 ,Celular VARCHAR(15)
-,DtExpiracao DATE not null DEFAULT '1900-01-01'
+,DtExpiracao DATE not null CONSTRAINT dfCoordenadorDtExpiracao DEFAULT ('1900-01-01')
 ,CONSTRAINT pkCoordenador PRIMARY KEY (IdCoordenador)
 ,CONSTRAINT uqCoordenadorLogin UNIQUE (Login)
 ,CONSTRAINT uqCoordenadorEmail UNIQUE (Email)
