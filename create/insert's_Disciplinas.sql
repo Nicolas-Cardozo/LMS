9df04fab6651295a04a3991eb6a2c1825b637cd2
Michael-Santana <36283338+Michael-Santana@users.noreply.github.com>
2018-04-14 23:19:52 -0300

    Create insert's_Disciplinas.sql

---
+++ create/insert's_Disciplinas.sql
@@ -0,0 +1,44 @@
+INSERT INTO Disciplina
+VALUES
+('1','Linguagem SQL',GETDATE(),'Conduzir o processo construtivo do conhecimento sobre a Linguagem de Consulta Estruturada - SQL.','80','Construir banco de dados, definindo
+seus relacionamentos e utilizando as técnicas e linguagens adequadas. ','Aplicar as técnicas e linguagens na construção de tabelas com base em modelos de banco de dados previamente definidos e manipular as informações do banco de dados,
+de acordo com as necessidades detectadas junto
+ao cliente.  ','Tecnologias emergentes de mercado que serão aplicadas em laboratório. ','Linguagens de Consulta Estrutura: SQL, Subconjuntos da Linguagem SQL:  (DML, DDL, DCL, DQL), Gerenciando Tabelas com SQL (comandos CREATE, INSERT, UPDATE e DELETE), Consultas e operações em tabelas (comando SELECT)',
+'DATE, C. J. Introdução a sistemas de bancos de dados. Rio de Janeiro: Campus, c2004. 865 p.
+MAYER, Roberto Carlos. Otimizando a performance de bancos de dados relacionais. Rio de
+Janeiro: Axcel Books, c2001. 158 p.
+RAMAKRISHNAN, Raghu; GEHRKE, Johannes. Sistemas de gerenciamento de banco de
+dados. São Paulo: McGraw-Hill, 2008. 884 p.','ELMASRI, Ramez; NAVATHE, Sham. Sistemas de banco de dados. 4. ed. São Paulo: Pearson,
+c2005. 724 p.
+GARCIA-MOLINA, Hector; ULLMAN, Jeffrey D. Implementação de sistemas de bancos de
+dados. Rio de Janeiro: Campus, 2001. 685 p.
+HEMRAJANI, Anil. Desenvolvimento ágil em Java com Spring, Hibernate e Eclipse. São
+Paulo: Prentice Hall, c2007. 290 p.
+SILBERSCHATZ, Abraham; KORTH, Henry F; SUDARSHAN, S. Sistema de banco de dados.
+Rio de Janeiro: Campus, 2006. 781 p.', '70', '30' );
+
+INSERT INTO Disciplina
+VALUES
+('2','Tecnologia Web',GETDATE(),'Fechada','Apresentar diferentes maneiras de desenvolver sistemas e ferramentas web com tecnologias mais relevantes
+para o mercado.','80','Desenvolvimento de Paginas Web em Front-End (exemplo: HTML, CSS,
+JavaScript, Flashetc).','Conhecer uma linguagem de programação para Web, Construir um aplicativo web com conexão a um banco de dados,Construir e validar formulários em HTML5, utilizando CSS e JavaScript, Entender a diferença entre client side e server side',
+'Noções básicas de programação para web; Programação client side versus server side;
+introdução à programação voltada para WEB; conceitos básicos sobre programação
+orientada a objetos em linguagem WEB; conexão em banco de dados; desenvolvimento de
+uma aplicação web com conexão à um banco de dados relacional; introdução aos principais
+Frameworks.','Formulários em HTML5, Utilização de CSS para formatação do layout do site, Utilização de JavaScript para validações do formulário, Uso de uma linguagem de programação para Web,','KRUG, Steve. Não me faça pensar: uma abordagem de bom senso à usabilidade na web. 2. ed. Rio de
+Janeiro: Alta Books, 2008. 201 p.
+GABARRO, Steven A. Web Application Design and Implementation: Apache 2, Php5, Mysql, Javascript, and
+Linux/UNIX (Quantitative Software Engineering Series). 1. ed. Wiley-IEEE Computer Society Pr.
+Disponível em: http://ieeexplore.ieee.org/xpl/bkabstractplus.jsp?bkn=5989430
+MUTO, Cláudio Adonai. PHP & MySQL: guia introdutório. 2. ed. Rio de Janeiro: Brasport, 2004. 326 p. + CDROM','ANSELMO, Fernando. Tudo sobre a JSP: com o NetBeans em aplicações distribuídas. Florianópolis: Visual
+Books, 2005. 222 p.
+BASHAM, Bryan; SIERRA, Kathy; BATES, Bert. Use a cabeça!: Servlets & JSP. Rio de Janeiro: Alta Books,
+2005. 534 p.
+BOMFIM JUNIOR, Francisco Tarcizo. JSP: a tecnologia java na internet . 2. ed. São Paulo: Érica, 2003. 404 p.
+GONÇALVES, Edson. Desenvolvendo aplicações web com JSP, Servlets, Javaserver faces, Hibernate,
+EJB 3 persistence e AJAX. Rio de Janeiro, RJ: Ciência Moderna, 2007. 736 p.
+NIEDERAUER, Juliano. Web interativa com Ajax e PHP. São Paulo: Novatec, 2007. 287 p.
+PESSOA, Márcio. Segurança em PHP: desenvolva programas PHP com alto nível de segurança e aprenda
+como manter os servidores web livres de ameaças. São Paulo, SP: Novatec, 2007. 151 p.
+','75','25');
