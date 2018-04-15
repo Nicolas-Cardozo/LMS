a383b545120045b3bfb9b96a66bac9e480e65e2e
Michael-Santana <36283338+Michael-Santana@users.noreply.github.com>
2018-04-14 23:19:13 -0300

    Create insert_curso.sql

---
+++ create/insert_curso.sql
@@ -0,0 +1,11 @@
+insert into curso
+values
+('Administrção'),
+('Análise e Desenvolvimento de Sistemas'),
+('Banco de Dados'),
+('Gestão de Tecnologia da informação'),
+('Jogos Digitais'),
+('Processos Gerenciais'),
+('Produção Multimídia'),
+('Redes de Computadores'),
+('Sistemas de Informação');
